2a71c858bee3bc1e2e75b025e183cde5616d4961
BRITVA_KROT_TG <vsevolodvolin6@gmail.com>
2026-05-19 13:35:00 +0300

    Add user creation, score update, and task completion
    Implement user management and scoring functions with Supabase integration.

--- app.js
+++ app.js
@@ -1,3 +1,77 @@
+api/app.js
+import supabase from '@/superbase'
+import { useTelegram } from '@/telegram'
+import { useScoreStore } from '../stores/score'
+
+const { user } = useTelegram()
+
+const MY_ID = user?.id ?? 'YOUR DEV ID' // Для разработки. В вебе нет user.id — он появляется только тогда, когда приложение запущено в рамках Telegram
+
+// Авторизация пользователя
+export async function getOrCreateUser() {
+  const potentialUser = await supabase
+    .from('users')
+    .select()
+    .eq('telegram', MY_ID)
+
+  // Проверяем, существует ли уже текущий пользователь
+  if (potentialUser.data.length !== 0) {
+    return potentialUser.data[0]
+  }
+  
+  // Если нет, то создаем нового
+  const newUser = {
+    telegram: MY_ID,
+    friends: {},
+    tasks: {},
+    score: 0,
+  }
+
+  await supabase.from('users').insert(newUser)
+  return newUser
+}
+
+// Добавляем обновление счета у текущего пользователя
+export async function updateScore(score) {
+  await supabase.from('users').update({ score }).eq('telegram', MY_ID)
+}
+
+// Завершаем задачу и начисляем бонусные баллы за ее выполнение
+export async function completeTask(user, task) {
+  await supabase
+    .from('users')
+    .update({ tasks: { ...user.tasks, [task.id]: true } })
+    .eq('telegram', MY_ID)
+
+  const score = useScoreStore()
+  const newScore = score.score + task.amount
+  await updateScore(newScore)
+  score.setScore(newScore)
+}
+
+// Регистрируем реферала
+export async function registerRef(userName, refId) {
+// Получаем данные пользователя, который поделился своей реферальной ссылкой
+const { data } = await supabase.from('users').select().eq('telegram', refId)
+
+  const refUser = data[0]
+
+  // Добавляем нас в список его рефералов, а также начисляем баллы
+  await supabase
+    .from('users')
+    .update({
+      friends: { ...refUser.friends, [MY_ID]: userName },
+      score: refUser.score + 50,
+    })
+    .eq('telegram', refId)
+}
+
+// Получаем список всех задач (он статический)
+export async function getTasks() {
+  const { data } = await supabase.from('tasks').select('*')
+
+  return data
+}
 // app.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
